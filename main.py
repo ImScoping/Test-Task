44acfc646691302b28fc4469ddd591e8e87188f1
Scop <dylkoiliya@gmail.com>
2021-11-03 13:39:52 +0300

    Обработаны возможные ошибки

--- main.py
+++ main.py
@@ -13,56 +13,70 @@ TASKS_PATH = BASE_PATH + '\\tasks\\'
 users_tasks = {}
 
 
-def create_file(user) -> None:
+def get_response(url: str) -> list:
+    """
+    Функция получения данных по URL с обработкой ошибок
+    :param url: URL запроса
+    :return: список словарей информации о пользователях/задачах
+    """
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        raise Exception('Ошибка получения данных запроса')
+
+
+def create_file(user: dict):
     """
     Функция создания файла для пользователя
     :param user: словарь данных пользователя
-    :return: None
     """
+    global users_tasks
+
     text_field = ''  # Формирование содержимого файла
     text_field += f"Отчет для {user['company']['name']}.\n"  # Первая строка
-    text_field += f"{user['name']} < {user['email']}> "  # Вторая строка
+    text_field += f"{user['name']} <{user['email']}> "  # Вторая строка
 
     current_date = datetime.now().strftime("%d.%m.%Y %H:%M")
     text_field += f"{current_date}\n"  # Добавление даты в нужном формате
+    text_field += f"Всего задач: "
+    if user['id'] in users_tasks.keys():
+        completed_tasks, left_tasks = users_tasks[user['id']]
+        completed_tasks_count, left_tasks_count = len(completed_tasks), len(left_tasks)
 
-    completed_tasks, left_tasks = users_tasks[user['id']]
-    completed_tasks_count, left_tasks_count = len(completed_tasks), len(left_tasks)
+        text_field += f"{completed_tasks_count + left_tasks_count}\n"  # Третья строка
+        text_field += f"\nЗавершенные задачи ({completed_tasks_count}):\n"
+        text_field += "".join([f"{completed_task}\n" for completed_task in completed_tasks])
+        # for completed_task in completed_tasks:
+        #     text_field += f"{completed_task}\n"
 
-    text_field += f"Всего задач: {completed_tasks_count + left_tasks_count}\n"  # Третья строка
-    text_field += f"\nЗавершенные задачи ({completed_tasks_count}):\n"
-    text_field += str([f"{completed_task}\n" for completed_task in completed_tasks])
-    text_field += f"\nОставшиеся задачи ({left_tasks_count}):\n"
-    text_field += str([f"{left_task}\n" for left_task in left_tasks])
+        text_field += f"\nОставшиеся задачи ({left_tasks_count}):\n"
+        text_field += "".join([f"{left_task}\n" for left_task in left_tasks])
+        # for left_task in left_tasks:
+        #     text_field += f"{left_task}\n"
+    else:  # Обработка отсутствия задач у пользователя
+        text_field += '0'
 
-    with open(f"{TASKS_PATH}{user['username']}.txt", 'w') as new_file:
+    with open(os.path.join(TASKS_PATH, user['username'] + '.txt'), 'w') as new_file:
         new_file.write(text_field)
 
 
-def rename_file(old_name) -> None:
+def rename_file(old_name: str):
     """
     Функция которая переименовывает исходный файл
     :param old_name: первоначальное имя файла (пользователя без txt)
-    :return: None
     """
-    old_path = f"{TASKS_PATH}{old_name}.txt"
+    old_path = os.path.join(TASKS_PATH, old_name + '.txt')
     with open(old_path, 'r') as file:
         text = file.read()
 
-    old_time = re.search(r"> (.*)\n", text).group(1)
-    # old_time = "".join(['-' if c in ['.', ':'] else 'T' if c == ' ' else c for c in old_time])
-    time_fixed = ''
-    for letter in old_time:
-        if letter in ['.',':']:
-            time_fixed += '-'
-        elif letter == ' ':
-            time_fixed += 'T'
-        else:
-            time_fixed += letter
+    old_time = re.search(r"> (.*)\n", text).group(1)  # Поиск даты в отчете
+    time_fixed = datetime.strptime(old_time, '%d.%m.%Y %H:%M').strftime('%d-%m-%YT%H-%M')  # Смена формата даты
     new_path = f"{TASKS_PATH}old_{old_name}_{time_fixed}.txt"
     try:
         os.rename(old_path, new_path)
     except FileExistsError:
+        # print(f"Файл с именем {old_path} отсутствует и будет создан заново")
         pass
 
 
@@ -79,12 +93,12 @@ def create_dir() -> bool:
         return True
 
 
-def fill_users_tasks() -> None:
+def fill_users_tasks():
     """
     Функция заполнения структуры users_tasks {'id': ([tasks_completed][tasks_left])}
-    :return: None
     """
-    tasks_list = requests.get(TODOS_URL).json()
+    global users_tasks
+    tasks_list = get_response(TODOS_URL)
     for task in tasks_list:
         try:
             task_title = task['title'] if len(task['title']) <= 48 else f"{task['title'][0:48]}..."
@@ -92,33 +106,28 @@ def fill_users_tasks() -> None:
             new_tasks = users_tasks.get(task['userId'], ([], []))
             new_tasks[0 if task['completed'] else 1].append(task_title)
             users_tasks.update({task['userId']: new_tasks})
-        except KeyError:
-            pass
+        except KeyError as e:
+            print(f"В записи отсутствует поле {e.args[0]}")
 
 
-def create_report() -> None:
+def create_report():
     """
     Функция создания отчета
-    :return:
     """
-    users_list = requests.get(USERS_URL).json()
+    users_list = get_response(USERS_URL)
     fill_users_tasks()
     if create_dir():
         for user in users_list:
             create_file(user)
     else:
-        print('Директория уже есть')
         for user in users_list:
-            if os.path.exists(f"{TASKS_PATH}{user['username']}.txt"):
+            if os.path.exists(os.path.join(TASKS_PATH, user['username'] + '.txt')):
                 rename_file(user['username'])
             create_file(user)
 
 
 def main():
-    date1 = datetime.now()
-    fill_users_tasks()
     create_report()
-    print(datetime.now() - date1)
 
 
 if __name__ == '__main__':
